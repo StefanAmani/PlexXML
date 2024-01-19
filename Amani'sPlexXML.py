9114f903295d0c092462268a0a5b61cedc6c751e
StefanAmani <107449453+StefanAmani@users.noreply.github.com>
2024-01-19 08:31:19 -0600

    Amani'sPlexXML.py

--- Amani'sPlexXML.py
+++ Amani'sPlexXML.py
@@ -1,6 +1,21 @@
 import os
 import xml.etree.ElementTree as ET
 
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
 # Create the file structure
 movie = ET.Element('movie')
 
@@ -29,6 +44,9 @@ while True:
     add_another = input("Would you like to add another actor? (yes/no): ")
     if add_another.lower() != 'yes':
         break
+
+# Indent the XML file
+indent(movie)
 
 # Create the XML file
 tree = ET.ElementTree(movie)
